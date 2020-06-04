735399fa0a50670a31a103da84f4033df346ee5d
Forever4sleep <tema.beloeerov.c@gmail.com>
2020-06-04 12:48:50 +0300

    The last commit was wrong so the changes of the database_manage.py were not applied

--- db_utils/database_manage.py
+++ db_utils/database_manage.py
@@ -36,24 +36,17 @@ class UsersLists(DbManage):
 
     @staticmethod
     @query_execution(do_commit=False)
-    def get_user_lists(self, id):
-        self.current_cursor.execute(
-            r"select name from lists where user_id = {id}"
+
+    def get_fields_from_lists(fields, condition):
+        UsersLists.current_cursor.execute(
+            f"select {fields} from lists where {condition}"
         )
 
-    @staticmethod
-    @query_execution(do_commit=False)
-
-    def get_words_from_list(self, list_id):
-        self.currect_cursor.execute(
-            r"select words from lists where id = {list_id}"
-        )
-        return self.current_cursor.fetchone()
 
     @staticmethod
     @query_execution(do_commit=True)
 
-    def add_list(user_id, list_name, last_update, interval=60):
+    def add_list(user_id, list_name, last_update, interval=60): 
         next_id_query = '(select max(id) + 1 from lists)'
 
         UsersLists.current_cursor.execute(
@@ -61,29 +54,22 @@ class UsersLists(DbManage):
             (next_id_query, user_id, list_name, interval, last_update)
         )
 
+
     @staticmethod
     @query_execution(do_commit=True)
 
-    def add_words_to_list(self, list_id, words):
-        self.current_cursor.execute(
-            r"update lists add words = '{words}' where id = {list_id}"
+    def add_words_to_list(list_id, words):
+        UsersLists.current_cursor.execute(
+            f"update lists add words = '{words}' where id = {list_id}"
         )
         UsersLists.current_cursor.fetchone()
     
-    @staticmethod
-    @query_execution(do_commit=False)
-
-    def get_all_users_lists(self, user_id):
-        self.current_cursor.execute(
-            r"select count(id) from lists where user_id = {user_id}"
-        )
-        return self.current_cursor.fetchone()
 
     @staticmethod
     @query_execution(do_commit=False)
 
-    def get_listID_by_name(self, list_name):
-        self.current_cursor.execute(
-            r"select id from lists where name = lower({list_name})"
+    def is_there_same_list(list_name, user_id):
+        UsersLists.current_cursor.execute(
+           f"select id from lists where name = '{list_name}' and user_id = {user_id}"
         )
-        return self.current_cursor.fetchone()
+        return UsersLists.current_cursor.fetchone()
